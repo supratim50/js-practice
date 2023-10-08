fd55f7c8f273d9d2f02c98ba19537fcbb821cd0f
Poor Coders <64735940+supratim50@users.noreply.github.com>
2023-10-08 14:30:31 +0000

    Promise practice

---
+++ 01_practices/promise.js
@@ -0,0 +1,50 @@
+const promiseOne = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Async task is complete!");
+        resolve();
+    }, 1000)
+})
+
+promiseOne.then(function() {
+    console.log("Promise Consume!");
+})
+
+new Promise((resolve, reject) => {
+    setTimeout(() =>{
+        console.log("Async task 2!");
+        resolve();
+    }, 1000)
+})
+.then(() => {
+    console.log("Promise Consume 2!")
+})
+
+const promiseThree = new Promise((resolve, reject) =>{
+    setTimeout(() => {
+        resolve({username: "Chai", email: "chai@example.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user) {
+    console.log(user);
+})
+
+const promiseFour = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        let error = false;
+        if(!error) {
+            resolve({username: "HItesh", password: "123"})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
+
+promiseFour.then((user) => {
+    console.log(user);
+    return user.username;
+}).then((username) => {
+    console.log(username);
+}).catch(function(error) {
+    console.log(error);
+})
