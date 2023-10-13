170a79e4a9743db6ee7fea20a848bcb5e2eee206
Poor Coders <64735940+supratim50@users.noreply.github.com>
2023-10-13 15:44:17 +0000

    Fetch with async await

--- 01_practices/fetch.js
+++ 01_practices/fetch.js
@@ -1,10 +1,21 @@
 const res = fetch("https://api.github.com/users/supratim50")
 
 res.then((res) => {
-    console.log("---------------------", res);
+    // console.log("---------------------", res);
     return res.json()
 })
 .then((res) => {
-    console.log("=========================",res)
+    // console.log("=========================",res)
 })
 
+// fetch with async await 
+
+const getData = async () => {
+    const res = await fetch("https://api.github.com/users/supratim50")
+    // console.log(await res.json());
+    // or
+    const resTwo = await res.json();
+    console.log((resTwo));
+}
+
+getData()
